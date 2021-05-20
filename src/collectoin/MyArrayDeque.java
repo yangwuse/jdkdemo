1c63b5f4942ae5a4ad8f3e562f436fde8c64c848
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-05-20 15:45:17 +0800

    ok

--- src/collectoin/MyArrayDeque.java
+++ src/collectoin/MyArrayDeque.java
@@ -1,10 +1,127 @@
 package collectoin;
 
+import java.rmi.dgc.VMID;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 /**
  * ArrayDeque demo
  * @author yangWu
  * @date 2021/5/18 3:43 下午
  * @version 1.0
  */
-public interface MyArrayDeque<E>  {
+public class MyArrayDeque<E> {
+    private Object[] elem;
+    private int head;
+    private int tail;
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    public MyArrayDeque() {
+        elem = new Object[5 + 1];
+    }
+    public MyArrayDeque(int cap) {
+        cap = (cap < 1)
+            ? 1
+            : (cap - MAX_ARRAY_SIZE > 0)
+                ? MAX_ARRAY_SIZE
+                : cap;
+        elem = new Object[cap];
+    }
+    public void addFirst(E e) {
+        Objects.requireNonNull(e);
+        head = dec(head, elem.length);
+        elem[head] = e;
+        if (head == tail)
+            grow(1);
+    }
+    public void addLast(E e) {
+        Objects.requireNonNull(e);
+        elem[tail] = e;
+        tail = inc(tail, elem.length);
+        if (tail == head)
+            grow(1);
+    }
+    private int dec(int i, int mod) {
+        if (--i < 0) i = mod - 1;
+        return i;
+    }
+    private int inc(int i, int mod) {
+        if (++i >= mod) i = 0;
+        return i;
+    }
+    private void grow(int needed) {
+        final int oldCapacity = elem.length;
+        int jump = (oldCapacity < 64) ? oldCapacity + 2 : (oldCapacity >> 1);
+        int newCapacity = oldCapacity + jump;
+        if (jump < needed || newCapacity - MAX_ARRAY_SIZE > 0)
+            newCapacity = newCapacity(needed, jump);
+        elem = Arrays.copyOf(elem, newCapacity);
+        if (tail < head || (tail == head && elem[head] != null)) {
+            int newSpace = newCapacity - oldCapacity;
+            System.arraycopy(elem, head, elem, head + newSpace
+                , oldCapacity - head);
+            for (int i = head, to = (head += newSpace); i < to; i++)
+                elem[i] = null;
+        }
+    }
+    private int newCapacity(int needed, int jump) {
+        final int oldCapacity = elem.length;
+        int minCapacity = needed + oldCapacity;
+        if (minCapacity - MAX_ARRAY_SIZE > 0) {
+            if (minCapacity < 0)
+                throw new IllegalStateException("to large");
+            return Integer.MAX_VALUE;
+        }
+        if (needed > jump)
+            return minCapacity;
+        return (jump + oldCapacity - MAX_ARRAY_SIZE > 0)
+            ? MAX_ARRAY_SIZE
+            : jump + oldCapacity;
+    }
+    public E pollFirst() {
+         E e = (E) elem[head];
+         if (e != null) {
+             elem[head] = null;
+             head = inc(head, elem.length);
+         }
+         return e;
+    }
+    public E pollLast() {
+         tail = dec(tail, elem.length);
+         E e = (E) elem[tail];
+         if (e != null)
+             elem[tail] = null;
+         return e;
+    }
+    public E getFirst() {
+        return (E) elem[head];
+    }
+    public E getLast() {
+        tail = dec(tail, elem.length);
+        return (E) elem[tail];
+    }
+
+    public static void main(String[] args) {
+        MyArrayDeque<Integer> deque = new MyArrayDeque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addLast(4);
+        deque.addLast(5);
+        deque.addLast(6);
+        deque.addLast(7);
+
+        deque.pollFirst();
+        deque.pollFirst();
+
+        deque.pollLast();
+        deque.pollLast();
+        deque.pollLast();
+        deque.pollLast();
+        deque.pollLast();
+        deque.pollLast();
+
+
+    }
 }
---
+++ src/collectoin/Test.java
@@ -0,0 +1,35 @@
+package collectoin;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ *
+ * @author yangWu
+ * @date 2021/5/18 9:18 下午
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>(5);
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        deque.addLast(10);
+        deque.addLast(20);
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.pollFirst();
+//        int[] a = new int[]{0, 0, 0, 1, 2, 3};
+//        a = Arrays.copyOf(a, 10);
+//        System.arraycopy(a, 3, a, 7, 3);
+//        System.out.println();
+    }
+}
